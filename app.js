074311509cf8fe9b0bb2e1e7a9d716a42e847a00
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-06-17 19:23:49 -0300

    confirmacion de la rama

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require ('express');
-const path = require ('path');
-const app = express ();
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const publicPath = path.resolve(__dirname,'./public');
+const publicPath = path.resolve(__dirname, "./public");
 
-app.use( express.static(publicPath) );
+app.use(express.static(publicPath));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
 
 app.listen(8080, () => {
-    console.log("Servidor corriendo en el puerto 8080");
+  console.log("Servidor corriendo en el puerto 8080");
 });
