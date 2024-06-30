d9904000d5ccaccf77ffdb28669274371d94c2a4
MarcosFlores178 <marcosfabianflores@gmail.com>
2024-06-30 12:49:30 -0300

    restore

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express ();
 const publicPath = path.resolve(__dirname,'./public');
 
 app.use( express.static(publicPath) );
+app.get('/',(req, res) => res.sendFile(path.resolve(__dirname, 'views/home.html')));
 
 app.listen(8080, () => {
     console.log("Servidor corriendo en el puerto 8080");
 });
+
