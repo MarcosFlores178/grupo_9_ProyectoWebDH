e4ae41e7158740dbaa73b45c8dca2d3f6e136876
MarcosFlores178 <marcosfabianflores@gmail.com>
2024-07-02 15:57:22 -0300

    Commit final

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express ();
 const publicPath = path.resolve(__dirname,'./public');
 
 app.use( express.static(publicPath) );
-app.get('/',(req, res) => res.sendFile(path.resolve(__dirname, 'views/home.html')));
+app.get('/',(req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
 
 app.listen(8080, () => {
     console.log("Servidor corriendo en el puerto 8080");
