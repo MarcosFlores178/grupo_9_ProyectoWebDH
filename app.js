b597f7ab3c06633da5e425bb73d855fee8ef48df
Marcos Flores <85637009+MarcosFlores178@users.noreply.github.com>
2024-07-04 19:33:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const path = require("path");
 const app = express();
 
 
-app.use( express.static(publicPath) );
-app.get('/',(req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+  res.sendFile(path.resolve(__dirname, 'views/login.html'));
 });
-app.get("/register",(req, res)=>{
-    res.sendFile(path.resolve("./views/register.html"));
-})
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
 
 app.listen(8080, () => {
   console.log("Servidor corriendo en el puerto 8080");
