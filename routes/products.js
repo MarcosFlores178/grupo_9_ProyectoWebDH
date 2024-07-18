f32562f383a1b0e15f7e449dd4f9028852300d0f
cgastonq <cgastonquintero@gmail.com>
2024-07-18 11:51:08 -0300

    modificacion products.js

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 
-router.get("/details-product", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'details-product.ejs'));
+router.get('/details-product', (req, res) => {
+    res.render('products/details-products');
 });
-  
-router.get("/shopping-cart", (req, res) =>{
-   res.sendFile(path.resolve(__dirname,'shopping-cart.ejs'));
+
+router.get('/shoping-card', (req, res) => {
+    res.render('products/register');
 });
 
 module.exports = router;
