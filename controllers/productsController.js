fa47d8f4fffb9fc1359fb894cce65a0cb45d640c
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-07-25 20:38:40 -0300

    comienzo tarea CRUD productos - sprint 4

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,14 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const productosFile = path.join(__dirname, "../data/products.json");
+const productos = JSON.parse(fs.readFileSync(productosFile, "utf-8"));
+
 const productsController = {
-    showDetails: (req, res) => {
-        res.render('products/details-product');
-    },
+  showDetails: (req, res) => {
+    res.render("products/details-product");
+  },
 
-    showShopCart: (req, res) => {
-        res.render('products/shop-cart');
-    },
+  showShopCart: (req, res) => {
+    res.render("products/shop-cart");
+  },
 
-    showAddProduct: (req, res) => {
-        res.render('products/addproduct');
-    }
+  showAddProduct: (req, res) => {
+    res.render("products/addproduct");
+  },
+  showAll: (req, res) => {
+    res.render("products/productos", { productos });
+  },
 };
 module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const productsController = require('../controllers/productsController');
+const express = require("express");
+const productsController = require("../controllers/productsController");
 const router = express.Router();
 
 // router.get("/details-product", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'details-product.ejs'));
 // });
-  
+
 // router.get("/shopping-cart", (req, res) =>{
 //    res.sendFile(path.resolve(__dirname,'shopping-cart.ejs'));
 // });
 
 router.get("/details-product", productsController.showDetails);
-  
+
 router.get("/shop-cart", productsController.showShopCart);
 
 router.get("/addproduct", productsController.showAddProduct);
 
+router.get("/", productsController.showAll);
 
 module.exports = router;
