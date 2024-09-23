f50c356d9429f1441411a6082d2ad1776f9df954
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-09-23 20:00:29 -0300

    crud de productos terminado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -51,11 +51,6 @@ const productsController = {
         usuario,
       });
     });
-
-    // const { id } = req.params;
-    // const productos = await dataSource.load();
-    // const product = productos.find((p) => p.id === id);
-    // res.render("products/details-product", { product, usuario });
   },
   showBrand: async (req, res) => {
     const { brand } = req.params.brand;
@@ -68,7 +63,11 @@ const productsController = {
   },
 
   showAddProduct: (req, res) => {
-    res.render("products/addproduct");
+    let marcas = db.Marca.findAll();
+    let talles = db.Talle.findAll();
+    Promise.all([marcas, talles]).then(([marcas, talles]) => {
+      res.render("products/addproduct", { marcas, talles });
+    });
   },
   addProduct: async (req, res) => {
     const imgProduct = req.file
@@ -77,16 +76,75 @@ const productsController = {
     db.Producto.create({
       nombre: req.body.nombre,
       descripcion: req.body.descripcion,
-      image: imgProduct,
+      imagen: imgProduct,
       color: req.body.color,
       precio: req.body.precio,
-      talle: req.body.talle,
-      marca: req.body.marca,
+      id_talle: req.body.talle,
+      id_marca: req.body.marca,
     }).then(() => {
       res.redirect("/products");
     });
   },
   showEditForm: (req, res) => {
+    let marcas = db.Marca.findAll();
+    let talles = db.Talle.findAll();
+    const producto = db.Producto.findByPk(req.params.id, {
+      include: [
+        {
+          model: db.Talle,
+          atributes: ["id", "descripcion"],
+          as: "talle",
+        },
+        {
+          model: db.Marca,
+          atributes: ["id", "descripcion"],
+          as: "marca",
+        },
+      ],
+    });
+    Promise.all([marcas, talles, producto]).then(
+      ([marcas, talles, producto]) => {
+        res.render("products/editproduct", { producto, marcas, talles });
+      }
+    );
+  },
+  editProduct: (req, res) => {
+    let imgProduct = "";
+    if (req.file?.filename) {
+      imgProduct = `${req.file.filename}`;
+    } else {
+      imgProduct = req.body.currentImage;
+    }
+    const { id } = req.params;
+
+    db.Producto.update(
+      {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        imagen: imgProduct,
+        color: req.body.color,
+        precio: req.body.precio,
+        id_talle: req.body.talle,
+        id_marca: req.body.marca,
+      },
+      {
+        where: { id: id },
+      }
+    )
+      .then(() => {
+        res.redirect(`/products/detail/${id}`);
+      })
+      .catch((error) => res.send(error));
+  },
+  showDelete: (req, res) => {
+    let usuario = req.session.user || null; // Asigna null si no hay usuario
+
+    // Verifica si el usuario tiene la propiedad admincomp y si es "admin"
+    if (usuario && usuario.admincomp === "admin") {
+      console.log("administrador:", usuario);
+    } else {
+      usuario = {};
+    }
     const pedidoProducto = db.Producto.findByPk(req.params.id, {
       include: [
         {
@@ -101,78 +159,19 @@ const productsController = {
         },
       ],
     }).then((producto) => {
-      return res.render("products/editproduct", {
+      return res.render("products/showDelete", {
         producto,
+        usuario,
       });
     });
   },
-  editProduct: (req, res) => {
-    let image = "";
-    const { name, description, colors, price, size, brand, currentImage } =
-      req.body;
-    if (req.file?.filename) {
-      image = `${req.file.filename}`;
-    } else {
-      image = req.body.currentImage;
-    }
-    const { id } = req.params;
-    this.productsList = dataSource.load();
-    const updateProduct = this.productsList.map((p) =>
-      p.id === id
-        ? {
-            id,
-            name,
-            description,
-            image,
-            colors,
-            price,
-            size,
-            brand,
-          }
-        : p
-    );
-    dataSource.save(updateProduct);
-    res.redirect(`/products/detail/${id}`);
-  },
   deleteProduct: async (req, res) => {
-    // const { id } = req.params;
-    // this.productsList = await dataSource.load();
-    // const { image } = this.productsList.find((p) => p.id === id);
-    // const filterProducts = this.productsList.filter((p) => p.id !== id);
-    // await dataSource.save(filterProducts);
-    // if (image !== "/images/default.jpg") {
-    //   await dataSource.removeFile(image);
-    // }
-
-    const { id } = req.params;
-    this.productsList = await dataSource.load();
-
-    // Encuentra el producto a eliminar
-    const productToDelete = this.productsList.find((p) => p.id === id);
-    if (!productToDelete) {
-      return res.status(404).send("Producto no encontrado");
-    }
-
-    // Filtra la lista de productos para eliminar el producto especificado
-    const filteredProducts = this.productsList.filter((p) => p.id !== id);
-    await dataSource.save(filteredProducts);
-
-    if (productToDelete.image && productToDelete.image !== "default.jpg") {
-      const imagePath = path.join(
-        __dirname,
-        "..",
-        "public",
-        "images",
-        "products",
-        productToDelete.image
-      );
-      fs.unlink(imagePath, (err) => {
-        if (err) {
-          console.error(`Error al eliminar la imagen: ${err}`);
-        }
-      });
-    }
-    res.redirect("/products");
+    let productId = req.params.id;
+    db.Producto.destroy({ where: { id: productId }, force: true }) // force: true es para asegurar que se ejecute la acción
+      .then(() => {
+        return res.redirect("/products");
+      })
+      .catch((error) => res.send(error));
   },
 };
 module.exports = productsController;
