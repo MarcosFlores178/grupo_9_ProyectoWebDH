5fb525dc030f17d359d46671d59beb1f6f9c11b2
Marcos Flores <85637009+MarcosFlores178@users.noreply.github.com>
2024-09-25 19:34:08 -0300

    Update rememberMiddleware.js

--- middlewares/rememberMiddleware.js
+++ middlewares/rememberMiddleware.js
@@ -14,18 +14,18 @@
 // module.exports = rememberMiddleware;
 
 
-const db = require("../database/models"); // Asegúrate de que este sea el path correcto a tus modelos Sequelize
+const db = require("../database/models"); 
 
 async function rememberMiddleware(req, res, next) {
-  // Si existe la cookie y no hay un usuario en la sesión
+  
   if (req.cookies && req.session.user == undefined) {
     try {
-      // Buscar al usuario en la base de datos usando el email guardado en la cookie
+      
       let user = await db.Usuario.findOne({
         where: { email: req.cookies.remember }
       });
 
-      // Si el usuario existe, lo guardamos en la sesión
+   
       if (user) {
         req.session.user = user;
       }
@@ -33,7 +33,7 @@ async function rememberMiddleware(req, res, next) {
       console.error("Error al buscar el usuario en la base de datos:", error);
     }
   } else {
-    // Si no hay cookies o el usuario ya está en la sesión, limpiamos la cookie
+   
     req.cookies = null;
   }
   
