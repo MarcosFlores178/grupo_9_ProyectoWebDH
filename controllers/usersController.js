f32608f4032fc8adc254dda1da92b0e655e40890
MarcosFlores178 <marcosfabianflores@gmail.com>
2024-09-25 19:21:46 -0300

    Backend usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,40 +1,63 @@
-const { validationResult } = require("express-validator");
-const { v4: uuidv4 } = require("uuid");
-const { countries } = require("countries-list");
+const { validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
+const { countries } = require('countries-list');
 const bcrypt = require("bcryptjs");
 const usersDataSource = require("../service/usersDataSource");
+const db = require('../database/models'); // Asegúrate de que esta ruta sea correcta
+const Producto = db.Producto;
+const Marca = db.Marca;
+const Usuario = db.Usuario;
+const Talle = db.Talle;
 const usersController = {
   userList: null,
   user: null,
   showLogin: (req, res) => {
-    res.render("users/login");
+    usuario = req.session.user
+    res.render("users/login", {usuario});
   },
   login: async (req, res) => {
     const { email, password } = req.body;
-    this.userList = await usersDataSource.load();
-    this.user = this.userList.find((u) => {
-      return u.email == email && bcrypt.compareSync(password, u.password);
-    });
-    req.session.user = this.user;
-    if (req.body.remember != undefined) {
-      res.cookie("remember", this.user.email, { maxAge: 3600 * 1000 });
+    try {
+      const usuario = await Usuario.findOne({
+        where: { email }
+      });
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+
+      }
+      console.log(req.body);
+      console.log(usuario);
+      const validPassword = await bcrypt.compare(password, usuario.password);
+      if (!validPassword) {
+        return res.status(404).json({ message: 'Contraseña incorrecta' });
+
+      }
+      req.session.user = usuario;
+      console.log('session:', req.session);
+      if (req.body.remember) {
+        res.cookie("remember", usuario.email, { maxAge: 60 * 10000 });
+      }
+      res.redirect("/users/perfil");
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error en el servidor' })
     }
-    res.redirect("/users/perfil");
   },
   logout: (req, res) => {
     req.session.destroy();
+    res.clearCookie('connect.sid');
     res.redirect("/");
   },
   showRegister: (req, res) => {
     // res.render("users/register");
-    const countryList = Object.values(countries).map((country) => country.name);
+    const countryList = Object.values(countries).map(country => country.name);
     // let errores = validationResult(req);
     // mapsDeError = undefined;
-    res.render("users/register", { countries: countryList, mapsDeError: {} });
+    res.render('users/register', { countries: countryList, mapsDeError: {} });
   },
   register: async (req, res) => {
     let errores = validationResult(req);
-    const countryList = Object.values(countries).map((country) => country.name);
+    const countryList = Object.values(countries).map(country => country.name);
     if (errores.isEmpty()) {
       const fotoUsuario = req.file ? `${req.file.filename}` : "default.jpg";
       const {
@@ -53,40 +76,139 @@ const usersController = {
       } = req.body;
       const newUser = {
         // id: uuidv4(),
-        id: crypto.randomUUID(),
+        // id: crypto.randomUUID(),
         nombre,
         apellido,
         dni,
         telefono,
         domicilio,
-        country,
-        nombreUsuario,
+        pais: country,
+        nombre_usuario: nombreUsuario,
         email,
         password: bcrypt.hashSync(password, 10),
-        admincomp,
-        tipoUsuario,
-        foto: fotoUsuario,
+        tipo_usuario: admincomp,
+        genero: tipoUsuario,
+        foto_perfil: fotoUsuario,
       };
-      console.log(newUser);
-      this.userList = await usersDataSource.load();
-      this.userList.push(newUser);
-      await usersDataSource.save(this.userList);
-      console.log(newUser);
-      res.redirect("/users/login");
+      // Intenta crear el nuevo usuario
+      try {
+        await db.Usuario.create(newUser);
+        res.redirect('/users/login'); // Redirige después de la creación exitosa
+      } catch (error) {
+        console.error(error);
+        if (!res.headersSent) {
+          res.status(500).send("Error al crear el usuario");
+        }
+      }
+
     } else {
+      // Si hay errores de validación, renderiza el formulario con los errores
       return res.render("users/register", {
         mapsDeError: errores.mapped(),
         old: req.body,
-
-        // arrayDeError: errores.array(),
-        countries: countryList,
+        countries: countryList
       });
     }
+
   },
   perfil: (req, res) => {
-    const usuario = req.session.user;
-    res.render("users/userPerfil", { usuario });
+    const usuario = req.session.user; // Obtén el usuario de la sesión
+    if (!usuario) {
+      return res.redirect('/users/login'); // Redirige si no hay usuario logueado
+    }
+    res.render("users/userPerfil", { usuario }); // Pasa el usuario a la vista
   },
+  showEdit: (req, res) => {
+    const countryList = Object.values(countries).map(country => country.name);
+    const usuario = req.session.user;
+    res.render("users/editarPerfil", { usuario, countries: countryList, mapsDeError: {} });
+  },
+  showEditCuenta: (req, res) => {
+    // const countryList = Object.values(countries).map(country => country.name);
+    const usuario = req.session.user;
+    res.render("users/editarCuenta", { usuario, mapsDeError: {} });
+  },
+  editUser: async (req, res) => {
+    let image = "";
+    const { nombre,
+      apellido,
+      dni,
+      telefono,
+      domicilio,
+      country,
+      tipoUsuario } = req.body;
+
+    // Verificar si hay una nueva imagen cargada
+    if (req.file?.filename) {
+      image = req.file.filename;
+    } else {
+      image = req.body.currentImage;
+    }
+
+    const { id } = req.params;
+
+    try {
+      // Buscar el producto por ID y actualizar los campos
+      await db.Usuario.update(
+        {
+          nombre,
+          apellido,
+          dni,
+          telefono,
+          domicilio,
+          pais: country,
+          genero: tipoUsuario,
+          foto_perfil: image
+        },
+        { where: { id } } // Condición para encontrar el producto por ID
+      );
+      //ACTUALIZA LA SESSION CON LOS NUEVOS DATOS EDITADOS
+      const userUpdated = await Usuario.findByPk(id); // Consulta los datos actualizados
+      req.session.user = userUpdated;
+      // console.log(req.body);
+      //res.redirect(`/users/perfil/${id}`);
+      res.redirect(`/users/perfil`);
+    } catch (error) {
+      console.error("Error al actualizar el usuario: ", error);
+      res.status(500).send("Error al actualizar el usuario");
+    }
+  },
+  editCuenta: async (req, res) => {
+    
+    const { nombreUsuario,
+      email,
+      admincomp,
+      password
+     } = req.body;
+
+    const { id } = req.params;
+    
+     console.log('console log de params', req.params);
+     console.log('console log de body', req.body);
+    try {
+      // Buscar el producto por ID y actualizar los campos
+      await db.Usuario.update(
+        {
+          nombre_usuario:nombreUsuario,
+          email,
+          tipo_usuario: admincomp,
+          password
+          
+        },
+        { where: { id } } // Condición para encontrar el producto por ID
+      );
+      //ACTUALIZA LA SESSION CON LOS NUEVOS DATOS EDITADOS
+      const userUpdated = await Usuario.findByPk(id); // Consulta los datos actualizados
+      req.session.user = userUpdated;
+      console.log(req.body);
+      //res.redirect(`/users/perfil/${id}`);
+      res.redirect(`/users/perfil`);
+    } catch (error) {
+      console.error("Error al actualizar el usuario: ", error);
+      res.status(500).send("Error al actualizar el usuario");
+    }
+  }
+
 };
 
 module.exports = usersController;
--- middlewares/rememberMiddleware.js
+++ middlewares/rememberMiddleware.js
@@ -1,14 +1,43 @@
-const usersDataSource = require("../service/usersDataSource.js");
+// const usersDataSource = require("../service/usersDataSource.js");
+// async function rememberMiddleware(req, res, next) {
+//   if (req.cookies && req.session.user == undefined) {
+//     let userList = await usersDataSource.load();
+//     let user = userList.find((u) => {
+//       return u.email == req.cookies.remember;
+//     });
+//     req.session.user = user;
+//   } else {
+//     req.cookies = null;
+//   }
+//   next();
+// }
+// module.exports = rememberMiddleware;
+
+
+const db = require("../database/models"); // Asegúrate de que este sea el path correcto a tus modelos Sequelize
+
 async function rememberMiddleware(req, res, next) {
+  // Si existe la cookie y no hay un usuario en la sesión
   if (req.cookies && req.session.user == undefined) {
-    let userList = await usersDataSource.load();
-    let user = userList.find((u) => {
-      return u.email == req.cookies.remember;
-    });
-    req.session.user = user;
+    try {
+      // Buscar al usuario en la base de datos usando el email guardado en la cookie
+      let user = await db.Usuario.findOne({
+        where: { email: req.cookies.remember }
+      });
+
+      // Si el usuario existe, lo guardamos en la sesión
+      if (user) {
+        req.session.user = user;
+      }
+    } catch (error) {
+      console.error("Error al buscar el usuario en la base de datos:", error);
+    }
   } else {
+    // Si no hay cookies o el usuario ya está en la sesión, limpiamos la cookie
     req.cookies = null;
   }
+  
   next();
 }
+
 module.exports = rememberMiddleware;
