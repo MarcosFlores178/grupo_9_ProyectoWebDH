cedf7490e584c29c7eacdbb9f77f0f035fa7fc09
MarcosFlores178 <marcosfabianflores@gmail.com>
2024-11-06 17:32:59 -0300

    Sweet alert de error en login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,10 +12,11 @@ const usersController = {
   userList: null,
   user: null,
   showLogin: (req, res) => {
+    let errorMessage = req.flash('ValErrorMessage')[0] || ''; // Recuperar el mensaje de error
     let successMessage = req.flash('successMessage')[0] || ''; //esta linea hace que si hay un mensaje de éxito, entonces se muestre en la vista. Y si no hay un mensaje de éxito, entonces no se muestre en la vista. Este req.flash viene desde el backend, desde el método el cual configura el success message y luego redirige el flujo hacia esta ruta/método. Y el [0] es para que se muestre el mensaje de éxito en la vista. Si no se pone el [0], entonces no se mostrará el mensaje de éxito en la vista. 
 
     usuario = req.session.user
-    res.render("users/login", {usuario, successMessage});
+    res.render("users/login", { usuario, successMessage, errorMessage });
   },
   login: async (req, res) => {
     const { email, password } = req.body;
@@ -24,15 +25,20 @@ const usersController = {
         where: { email }
       });
       if (!usuario) {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
+        req.flash('ValErrorMessage', 'Email o contraseña incorrectos');
+        const errorMessage = req.flash('ValErrorMessage')[0] || '';
+        // return res.status(404).json({ message: 'Usuario no encontrado' });
+        return res.render("users/login", { errorMessage, successMessage: '' });
 
       }
       console.log(req.body);
       console.log(usuario);
       const validPassword = await bcrypt.compare(password, usuario.password);
       if (!validPassword) {
-        return res.status(404).json({ message: 'Contraseña incorrecta' });
-
+        req.flash('ValErrorMessage', 'Email o contraseña incorrectos');
+        const errorMessage = req.flash('ValErrorMessage')[0] || '';
+        // return res.status(404).json({ message: 'Contraseña incorrecta' });
+        return res.render("users/login", { errorMessage , successMessage: ''});
       }
       req.session.user = usuario;
       console.log('session:', req.session);
@@ -96,7 +102,7 @@ const usersController = {
       try {
         await db.Usuario.create(newUser);
         req.flash('successMessage', 'Usuario registrado con éxito.'); //No sé a dónde va este mensaje. El mensaje que sale es el de el ejs
-      // res.redirect(`/users/perfil`);
+        // res.redirect(`/users/perfil`);
         res.redirect('/users/login'); // Redirige después de la creación exitosa
       } catch (error) {
         console.error(error);
@@ -121,7 +127,7 @@ const usersController = {
     if (!usuario) {
       return res.redirect('/users/login'); // Redirige si no hay usuario logueado
     }
-    res.render("users/userPerfil", { usuario, successMessage}); // Pasa el usuario a la vista
+    res.render("users/userPerfil", { usuario, successMessage }); // Pasa el usuario a la vista
   },
   showEdit: (req, res) => {
     const countryList = Object.values(countries).map(country => country.name);
@@ -139,66 +145,66 @@ const usersController = {
     // console.log(errorMessage);
   },
   editUser: async (req, res) => {
-    try{
-    let image = "";
-    let errores= validationResult(req);
-    const countryList = Object.values(countries).map(country => country.name); 
-    const { nombre,
-      apellido,
-      dni,
-      telefono,
-      domicilio,
-      country,
-      tipoUsuario } = req.body;
+    try {
+      let image = "";
+      let errores = validationResult(req);
+      const countryList = Object.values(countries).map(country => country.name);
+      const { nombre,
+        apellido,
+        dni,
+        telefono,
+        domicilio,
+        country,
+        tipoUsuario } = req.body;
 
-    // Verificar si hay una nueva imagen cargada
-    if (req.file?.filename) {
-      image = req.file.filename;
-    } else {
-      image = req.body.currentImage;
-    }
+      // Verificar si hay una nueva imagen cargada
+      if (req.file?.filename) {
+        image = req.file.filename;
+      } else {
+        image = req.body.currentImage;
+      }
 
-    const { id } = req.params;
-    let usuario = await db.Usuario.findByPk(id);
-    if (errores.isEmpty()) {
-      // Buscar el producto por ID y actualizar los campos
-      await db.Usuario.update(
-        {
-          nombre,
-          apellido,
-          dni,
-          telefono,
-          domicilio,
-          pais: country,
-          genero: tipoUsuario,
-          foto_perfil: image
-        },
-        { where: { id } } // Condición para encontrar el producto por ID
-      );
-      //ACTUALIZA LA SESSION CON LOS NUEVOS DATOS EDITADOS
-      const userUpdated = await Usuario.findByPk(id); // Consulta los datos actualizados
-      req.session.user = userUpdated;
-      // console.log(req.body);
-      //res.redirect(`/users/perfil/${id}`);
-      req.flash('successMessage', 'Perfil editado con éxito.');
-      res.redirect(`/users/perfil`);
-    } else {
-      // Si hay errores, renderizar la vista de edición con errores y datos viejos
-      req.flash('ValErrorMessage', 'Complete los campos requeridos');
+      const { id } = req.params;
+      let usuario = await db.Usuario.findByPk(id);
+      if (errores.isEmpty()) {
+        // Buscar el producto por ID y actualizar los campos
+        await db.Usuario.update(
+          {
+            nombre,
+            apellido,
+            dni,
+            telefono,
+            domicilio,
+            pais: country,
+            genero: tipoUsuario,
+            foto_perfil: image
+          },
+          { where: { id } } // Condición para encontrar el producto por ID
+        );
+        //ACTUALIZA LA SESSION CON LOS NUEVOS DATOS EDITADOS
+        const userUpdated = await Usuario.findByPk(id); // Consulta los datos actualizados
+        req.session.user = userUpdated;
+        // console.log(req.body);
+        //res.redirect(`/users/perfil/${id}`);
+        req.flash('successMessage', 'Perfil editado con éxito.');
+        res.redirect(`/users/perfil`);
+      } else {
+        // Si hay errores, renderizar la vista de edición con errores y datos viejos
+        req.flash('ValErrorMessage', 'Complete los campos requeridos');
         const errorMessage = req.flash('ValErrorMessage')[0] || '';
         const successMessage = req.flash('successMessage')[0] || '';
         console.log('errores:', errores.mapped());
         console.log('else de errores');
         // Recuperar el mensaje de éxito
-      return res.render("users/editarPerfil", {
-        usuario: usuario,
-        mapsDeError: errores.mapped(),
-        old: req.body, // Datos ingresados para que el formulario no se reinicie
-        countries: countryList,
-        successMessage,
-        errorMessage
-      });
-    }
+        return res.render("users/editarPerfil", {
+          usuario: usuario,
+          mapsDeError: errores.mapped(),
+          old: req.body, // Datos ingresados para que el formulario no se reinicie
+          countries: countryList,
+          successMessage,
+          errorMessage
+        });
+      }
     } catch (error) {
       console.error("Error al actualizar el usuario: ", error);
       req.flash('ValErrorMessage', 'Ocurrió un error. Intente nuevamente');
@@ -217,20 +223,21 @@ const usersController = {
       let hashedNewPassword;
       if (newP && confirmNewP) { //Esta linea hace que si newP y confirmNewP existen, entonces se ejecuta el siguiente bloque de codigo. Y si no existen, entonces no se ejecuta el siguiente bloque de codigo.
         if (newP !== confirmNewP) {
-            return res.render("users/editarPerfil", {
-                usuario: usuario,
-                mapsDeError: { confirmNewPassword: { msg: "Las contraseñas no coinciden" } },
-                old: req.body,
-            });
+          return res.render("users/editarPerfil", {
+            usuario: usuario,
+            mapsDeError: { confirmNewPassword: { msg: "Las contraseñas no coinciden" } },
+            old: req.body,
+          });
         }
         hashedNewPassword = await bcrypt.hash(newP, 10);
       }
-      console.log('Errores',errores.isEmpty());
+      console.log('Errores', errores.isEmpty());
       console.log(errores.mapped());
-      console.log('Pass hasheada',hashedNewPassword);
+      console.log('Pass hasheada', hashedNewPassword);
       if (errores.isEmpty()) {
         await db.Usuario.update(
-          { nombre_usuario: nombreUsuario,
+          {
+            nombre_usuario: nombreUsuario,
             tipo_usuario: admincomp,
             email,
             // password: bcrypt.hashSync(password, 10)
@@ -266,18 +273,18 @@ const usersController = {
     try {
       const usuario = req.session.user;
       const { currentPassword } = req.body;
-  
+
       // Aquí deberías obtener el usuario actual. Puedes hacerlo desde req.user si estás usando autenticación
       // O puedes buscarlo en la base de datos según alguna condición, por ejemplo, el id del usuario:
       const userId = usuario.id; // Si estás usando sesiones o autenticación por tokens
       const user = await db.Usuario.findByPk(userId); // Obtenemos el usuario desde la base de datos
       // console.log(usuario);
-      console.log('contraseña es:',user.password);
-      console.log('dni es:',user.dni);
-      console.log('current password es:',currentPassword);
+      console.log('contraseña es:', user.password);
+      console.log('dni es:', user.dni);
+      console.log('current password es:', currentPassword);
       // Comparamos la contraseña ingresada (password) con la contraseña almacenada en la base de datos
       const match = await bcrypt.compare(currentPassword, user.password);
-  
+
       if (match) {
         res.json({ valid: true });
       } else {
