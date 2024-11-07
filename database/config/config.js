4c3609c7aed34f8e267b8fc1e46aac89bb9c12ad
d1360d0990 <diego.d0990@gmail.com>
2024-11-07 18:12:32 -0300

    .

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: "",
-    database: "golazos2",
+    database: "golazo2",
     host: "127.0.0.1",
     dialect: "mysql",
   },
